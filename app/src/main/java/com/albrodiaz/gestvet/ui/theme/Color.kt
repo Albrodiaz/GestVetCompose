2feeb3d44fe647a83919a49557fe56cca798091b
Alberto <choketido@gmail.com>
2023-02-24 18:36:30 +0100

    Configuración del proyecto

---
+++ app/src/main/java/com/albrodiaz/gestvet/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.albrodiaz.gestvet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF009688)
+val PrimaryLightColor = Color(0xFF52C7B8)
+val PrimaryDarkColor = Color(0xFF00675B)
+val SecondaryColor = Color(0xFFFFC107)
+val SecondaryLightColor = Color(0xFFFFF350)
+val SecondaryDarkColor = Color(0xFFC79100)
---
+++ app/src/main/java/com/albrodiaz/gestvet/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.albrodiaz.gestvet.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = PrimaryColor,
+    primaryVariant = PrimaryDarkColor,
+    secondary = SecondaryColor
+)
+
+private val LightColorPalette = lightColors(
+    primary = PrimaryColor,
+    primaryVariant = PrimaryDarkColor,
+    secondary = SecondaryColor
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun GestVetTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
