09710441d1dcbd0e59068615997c3654ddfd7917
Albrodiaz <alrodiaz15@gmail.com>
2023-02-26 19:16:30 +0100

    Layout de pantalla para crear citas

--- app/src/main/java/com/albrodiaz/gestvet/ui/theme/Theme.kt
+++ app/src/main/java/com/albrodiaz/gestvet/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.albrodiaz.gestvet.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
@@ -75,7 +76,7 @@ private val DarkColors = darkColorScheme(
 @Composable
 fun GestVetTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colors = if (!useDarkTheme) {
         LightColors
@@ -85,6 +86,7 @@ fun GestVetTheme(
 
     MaterialTheme(
         colorScheme = colors,
-        content = content
+        content = content,
+        typography = typography
     )
 }
