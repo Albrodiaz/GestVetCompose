f3b0df6b6c5bfc69589e57645bc1e5e30fba5fb5
Albrodiaz <alrodiaz15@gmail.com>
2023-03-29 10:05:43 +0200

    Creo ficheros necesarios para el loginScreen y registerscreen

---
+++ app/src/main/java/com/albrodiaz/gestvet/ui/features/login/model/User.kt
@@ -0,0 +1,5 @@
+package com.albrodiaz.gestvet.ui.features.login.model
+
+data class User(
+    val name: String? = null
+)
---
+++ app/src/main/java/com/albrodiaz/gestvet/ui/features/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.albrodiaz.gestvet.ui.features.login.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+}
