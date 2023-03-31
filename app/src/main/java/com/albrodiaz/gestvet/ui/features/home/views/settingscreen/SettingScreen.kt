fa5a3798786482eaf1b3551f08ee334855a5abe3
Alberto <choketido@gmail.com>
2023-03-31 23:43:47 +0200

    Añado searchScreen, navegación, logout, strings y algunos arreglos en mainScreen

--- app/src/main/java/com/albrodiaz/gestvet/ui/features/home/views/settingscreen/SettingScreen.kt
+++ app/src/main/java/com/albrodiaz/gestvet/ui/features/home/views/settingscreen/SettingScreen.kt
@@ -1,29 +1,44 @@
 package com.albrodiaz.gestvet.ui.features.home.views.settingscreen
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.albrodiaz.gestvet.R
 import com.albrodiaz.gestvet.ui.features.home.viewmodels.settings.SettingsViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingScreen(
     settingsViewModel: SettingsViewModel = hiltViewModel(),
-    navToLogin: ()-> Unit
+    navToLogin: () -> Unit
 ) {
     Column(
         Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Cerrar Sesión", modifier = Modifier.clickable {
-            settingsViewModel.logOut()
-            navToLogin()
-        })
+        CenterAlignedTopAppBar(
+            title = { Text(text = "") },
+            actions = {
+                IconButton(
+                    onClick = {
+                        settingsViewModel.logOut()
+                        navToLogin()
+                    }
+                ) {
+                    Icon(
+                        Icons.Filled.Logout,
+                        contentDescription = stringResource(id = R.string.logout)
+                    )
+                }
+            }
+        )
+        Text(text = "Settings Screen")
     }
 }
